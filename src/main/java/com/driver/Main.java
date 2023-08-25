f3d3eb227d43fa2be7e7abe6c848c1e9ea39f7aa
anjana <anjana123@gmail.com>
2023-08-26 01:17:20 +0530

    Completed the polymorphysm task

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+  public static class Product {
+      public int product(int x, int y) {
+          return x*y;
+      }
+      int product(int x, int y, int z) {
+          return x*y*z;
+      }
+      public double product(double x, double y) {
+          return x*y;
+      }
+  }
+  public static void main(String args[]) {
+      Product p = new Product();
+      System.out.println(p.product(1,2));
+      System.out.println(p.product(1,2,3));
+      System.out.println(p.product(1.50,2.80));
+  }
 }
